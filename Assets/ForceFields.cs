0ee099428b97fa5a7bb32b51e7a7a34f2eaf9cf3
Saeed Safikhani <s.safikhani@outlook.com>
2019-11-03 19:27:41 +0100

    no message

---
+++ Assets/ForceFields.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceFields : MonoBehaviour
+{
+    public float NormalSpeed;
+    public float AngularSpeed;
+
+    [SerializeField] Vector3 ForceField;
+    [SerializeField] float Frequence;
+    Vector3 CurrentForceField;
+    float SinFun;
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        CurrentForceField = new Vector3(0f,0f,0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NormalSpeed = rigidBody.velocity.magnitude;
+        AngularSpeed = rigidBody.angularVelocity.magnitude;
+
+        if (Mathf.Sin(Time.deltaTime*Frequence) > 0f)
+        {
+            SinFun = 1f;
+        }
+        else
+        {
+            SinFun = -1f;
+        }
+        //CurrentForceField.x = ForceField.x *Mathf.Round(Mathf.Sin(Time.deltaTime*Frequence));
+        CurrentForceField.x = ForceField.x * SinFun;
+
+        rigidBody.AddForce(CurrentForceField);
+        
+    }
+}
---
+++ Assets/Scenes/A004/WateringFull.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+public class WateringFull : MonoBehaviour
+{
+    SkinnedMeshRenderer BlendShape;
+    [SerializeField] [Range (0.0f,100f)] float wettingPrecent = 0.0f;
+    [SerializeField] float wettingMultiplier = 1.0f;
+    [SerializeField] float DryingMultiplier = 1.0f;
+    [SerializeField] float DropTime = 1.0f;
+    [SerializeField] float CactusHealth = 100f;
+    [SerializeField] float CactusHitPoint = 10f;
+
+    bool underWater = false;
+    public GameObject WaterSplash;
+    public GameObject WaterDrop;
+    public GameObject WateronGround;
+
+    float dropWaitTime = 0f;
+
+    void Start()
+    {
+        BlendShape = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputKeys();
+        WaterCheck();
+        WateringMethod();
+    }
+
+    private void WaterCheck()
+    {
+
+    }
+
+    private void WateringMethod()
+    {
+        BlendShape.SetBlendShapeWeight(0, wettingPrecent);
+    }
+    
+    private void WaterDropping()
+    {
+        GameObject waterDrop = Instantiate(WaterDrop) as GameObject;
+        waterDrop.transform.position = transform.position;
+    }
+
+    private void GetInputKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow)) 
+        {
+            float verticalPosition = transform.position.y - 0.2f;
+            transform.position = new Vector3(transform.position.x,verticalPosition,transform.position.z);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            float verticalPosition = transform.position.y + 0.2f;
+            transform.position = new Vector3(transform.position.x,verticalPosition,transform.position.z);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Water")
+        {
+            Debug.Log("Cuctus is underwater");
+            if (!underWater)
+            {
+                // GameObject waterSplash = Instantiate(WaterSplash) as GameObject;
+                // waterSplash.transform.position = transform.position;
+            }
+            underWater = true;
+        }
+        if (collider.gameObject.tag == "CactusEnemy")
+        {
+            CactusHealth = CactusHealth - CactusHitPoint;
+            if (CactusHealth < 1)
+            {
+                Destroy(gameObject,0.001f);
+                if (wettingPrecent > 1)
+                {
+                    GameObject waterOnGround = Instantiate(WateronGround) as GameObject;
+                    waterOnGround.transform.position = transform.position;
+                }
+            }
+        }
+        if (collider.gameObject.tag == "FullWater")
+        {
+            Debug.Log("Collider Happend");
+            wettingPrecent = 100f;
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        Debug.Log("Cuctus is out of water");
+        underWater = false;
+    }
+
+}
